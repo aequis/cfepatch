6b01a6acd279aa2df2a47b6abb3be5e397f93d1c
Peter Ellsum <cfehunter@gmail.com>
2020-06-12 13:45:15 +0100

    Added support for disabling airstrikes on commando missions prior to the player having a base.

--- TIBERIANDAWN/CFEPATCHQUERY.cpp
+++ TIBERIANDAWN/CFEPATCHQUERY.cpp
@@ -8,3 +8,38 @@ bool CFE_Patch_Can_Enter_Override(const ObjectClass& object)
 	}
 	return false;
 }
+
+//Disable A10s in missions if the player has no buildings
+bool CFE_Patch_A10_Override(const ObjectTypeClass& object)
+{
+	if (ActiveCFEPatchConfig.DisableCommandoAirstrikes
+		&& GameToPlay == GAME_NORMAL
+		&& object.What_Am_I() == RTTI_AIRCRAFTTYPE
+		&& ((const AircraftTypeClass&)(object)).Type == AIRCRAFT_A10)
+	{
+		return CFE_Patch_House_Has_Any_Buildings_In_Map(*PlayerPtr);
+	}
+	return true;
+}
+
+bool CFE_Patch_House_Has_Any_Buildings_In_Map(HouseClass& house)
+{
+	if (house.ActiveBScan)
+	{
+		/*cfehunter 12/06/2020
+		*Certain missions give you buildings that are owned by you, but not yet discovered
+		*We need to account for this here.
+		*/
+		if (house.IsHuman && GameToPlay == GAME_NORMAL)
+		{
+			for (const void* rawBuilding : Buildings.ActivePointers)
+			{
+				const BuildingClass& building = *reinterpret_cast<const BuildingClass*>(rawBuilding);
+				if (building.House == &house && building.Is_Discovered_By_Player(&house))
+					return true;
+			}
+		}
+		else return true;
+	}
+	return false;
+}
