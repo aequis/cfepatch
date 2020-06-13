71b1bde56ebfa55580c1d7a3e9989c5a03999d65
Peter Ellsum <cfehunter@gmail.com>
2020-06-13 02:11:03 +0100

    Fix stop for clearing rally points on buildings that have active missions
    Repair pad queues now work without a rally point

--- TIBERIANDAWN/CFEPATCHQUERY.cpp
+++ TIBERIANDAWN/CFEPATCHQUERY.cpp
@@ -49,3 +49,8 @@ bool CFE_Patch_Is_Wall(const ObjectTypeClass& object)
 {
 	return object.What_Am_I() == RTTI_BUILDINGTYPE && static_cast<const BuildingTypeClass&>(object).IsWall;
 }
+
+bool CFE_Patch_Can_Have_Rally_Point(const ObjectClass& object)
+{
+	return object.What_Am_I() == RTTI_BUILDING && static_cast<const BuildingClass&>(object).Can_Have_Rally_Point();
+}
